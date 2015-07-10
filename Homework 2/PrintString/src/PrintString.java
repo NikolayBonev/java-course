58e613a2fa48538f291db0809482d29b81aae7f0
NikolayBonev <niki9442@abv.bg>
2015-07-10 14:41:40 +0300

    Homework 2

---
+++ Homework 2/PrintString/src/PrintString.java
@@ -0,0 +1,11 @@
+//ƒекларирайте променлива от тип String и отпечатайте следни€ стринг 
+//УI will read the УHarry PotterФ book collection.Ф 
+
+public class PrintString {
+
+	public static void main(String[] args) {
+		System.out.println("I will read the \"Harry Potter\" book collection.");
+
+	}
+
+}
