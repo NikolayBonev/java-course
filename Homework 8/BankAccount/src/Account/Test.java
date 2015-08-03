8cffbdce7f04c6826e05f945d66f266fede28557
NikolayBonev <niki9442@abv.bg>
2015-08-03 22:34:35 +0300

    Homework 8 Final

--- Homework 8/BankAccount/src/Account/Test.java
+++ Homework 8/BankAccount/src/Account/Test.java
@@ -1,16 +1,23 @@
 package Account;
 
 public class Test {
-	public static void main(String[] args) throws NegativeMoneyException {
-		
+	public static void main(String[] args) {
 		try {
 			PersonalAccount person = new PersonalAccount(3000.0, "Nikolay", "9412046740");
+//			person1 will throw NegativeMoneyException
+			PersonalAccount person1 = new PersonalAccount(-3000.0, "Lalka", "6512046740");
 			CoorporativeAccount company = new CoorporativeAccount(9600.17 , "Rumen", "7612046888");
+//			errorPer will throw InvalidAccountException
 			Object errorPer = "Alibaba";
+			
 			checkForInstance(person);
+			checkForInstance(person1);
 			checkForInstance(company);
 			checkForInstance(errorPer);
+			
 		} catch (InvalidAccountException exception) {
+			System.out.println("The exception is caught: " + exception);
+		} catch (NegativeMoneyException exception) {
 			System.out.println("The exception is caught: " + exception);
 		}
 	}
